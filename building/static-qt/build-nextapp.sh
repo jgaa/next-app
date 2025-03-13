8eaf13003605ed66c9207ae625441e56af174129
Jarle Aase <jarle@jgaa.com>
2025-03-13 11:46:27 +0200

    Only building the image if BUILD_IMAGE is unset

--- building/static-qt/build-nextapp.sh
+++ building/static-qt/build-nextapp.sh
@@ -2,11 +2,11 @@
 
 if [ -z ${BUILD_IMAGE+x} ]; then
     BUILD_IMAGE=qt-static
+    time docker build -t ${BUILD_IMAGE} . &&\
 fi
 
 chmod 0777 target
 
-time docker build -t qt-static . &&\
 time docker run --rm -v `pwd`/../../:/next-app:ro -v `pwd`/target:/target --name qt-static-build-nextapp -it ${BUILD_IMAGE} bash /next-app/building/static-qt/build-from-local-src.sh &&\
 echo "Successfully done!"
 
