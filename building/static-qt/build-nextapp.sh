b20bba088f1be5492cd3c5379f56494a4a11789d
Jarle Aase <jarle@jgaa.com>
2025-03-13 11:43:15 +0200

    Using the correct build image.

--- building/static-qt/build-nextapp.sh
+++ building/static-qt/build-nextapp.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
+if [ -z ${BUILD_IMAGE+x} ]; then
+    BUILD_IMAGE=qt-static
+fi
+
 chmod 0777 target
 
 time docker build -t qt-static . &&\
-time docker run --rm -v `pwd`/../../:/next-app:ro -v `pwd`/target:/target --name qt-static-build-nextapp -it qt-static bash /next-app/building/static-qt/build-from-local-src.sh &&\
-
+time docker run --rm -v `pwd`/../../:/next-app:ro -v `pwd`/target:/target --name qt-static-build-nextapp -it ${BUILD_IMAGE} bash /next-app/building/static-qt/build-from-local-src.sh &&\
 echo "Successfully done!"
 
