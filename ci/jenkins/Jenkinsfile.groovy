bc64852d85123d474fcd01957d3d6ed8b270a42c
Jarle Aase <jarle@jgaa.com>
2025-07-18 09:52:57 +0300

    Experimenting with windows build

--- ci/jenkins/Jenkinsfile.groovy
+++ ci/jenkins/Jenkinsfile.groovy
@@ -3,62 +3,66 @@ pipeline {
   /* no global agent: pick per-stage */
   agent none
 
-  // Assumes cmake, nsis, ninja and exists
-  //
-  //   choco install nsis
-  //   choco install ninja
-
   stages {
-   stage('Windows Build') {
-      agent { label 'windows' }
-      environment {
-        BUILD_DIR               = "${WORKSPACE}\\build"
-        VCPKG_ROOT              = "C:\\src\\vcpkg"
-        VCPKG_DEFAULT_TRIPLET   = "x64-windows-release"
-        CMAKE_GENERATOR_PLATFORM= "x64"
-        CMAKE_GENERATOR          = "Ninja"
-      }
-      steps {
-        checkout scm
-        bat 'git submodule update --init'
+   stage('Build') {
+      parallel {
 
-       bat """
-          @echo off
-          REM path to vswhere.exe
-          set "VSWHERE=%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe"
+      stage('Windows Build') {
+          // Assumes cmake, nsis, ninja and exists
+          //
+          //   choco install nsis
+          //   choco install ninja
 
-          if exist "%VSWHERE%" (
-            REM query latest VS install path into VSINSTALL
-            for /f "delims=" %%I in ('"%VSWHERE%" -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath') do (
-              set "VSINSTALL=%%I"
-            )
-            call "%%VSINSTALL%%\\VC\\Auxiliary\\Build\\vcvars64.bat"
-          ) else (
-            REM fallback: direct call to the known path
-            call "%ProgramFiles%\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvars64.bat"
-          )
+          agent { label 'windows' }
+          environment {
+            BUILD_DIR               = "${WORKSPACE}\\build"
+            VCPKG_ROOT              = "C:\\src\\vcpkg"
+            VCPKG_DEFAULT_TRIPLET   = "x64-windows-release"
+            CMAKE_GENERATOR_PLATFORM= "x64"
+            CMAKE_GENERATOR          = "Ninja"
+          }
+          steps {
+            checkout scm
+            bat 'git submodule update --init'
 
-          pushd %VCPKG_ROOT%
-          echo 🔄 Pulling latest vcpkg…
-          git pull
-          popd
+          bat """
+              @echo off
+              REM path to vswhere.exe
+              set "VSWHERE=%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe"
 
-          echo "Starting build..."
-          building\\static-qt-windows\\build-nextapp.bat
-        """
+              if exist "%VSWHERE%" (
+                REM query latest VS install path into VSINSTALL
+                for /f "delims=" %%I in ('"%VSWHERE%" -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath') do (
+                  set "VSINSTALL=%%I"
+                )
+                call "%%VSINSTALL%%\\VC\\Auxiliary\\Build\\vcvars64.bat"
+              ) else (
+                REM fallback: direct call to the known path
+                call "%ProgramFiles%\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvars64.bat"
+              )
 
-        script {
-          def ver = powershell(
-            returnStdout: true,
-            script: "Get-Content \"${env.BUILD_DIR}\\nextapp\\VERSION.txt\" -Raw"
-          ).trim()
-          env.NEXTAPP_VERSION = ver
-          echo "✅ NEXTAPP_VERSION=${ver}"
-        }
+              pushd %VCPKG_ROOT%
+              echo 🔄 Pulling latest vcpkg…
+              git pull
+              popd
 
-        archiveArtifacts artifacts: "build/*.exe", fingerprint: true
-      }
-    } // win
+              echo "Starting build..."
+              building\\static-qt-windows\\build-nextapp.bat
+            """
 
+            script {
+              def ver = powershell(
+                returnStdout: true,
+                script: "Get-Content \"${env.BUILD_DIR}\\nextapp\\VERSION.txt\" -Raw"
+              ).trim()
+              env.NEXTAPP_VERSION = ver
+              echo "✅ NEXTAPP_VERSION=${ver}"
+            }
+
+            archiveArtifacts artifacts: "build/*.exe", fingerprint: true
+          }
+        } // win
+      } parallel
+    } // Build
   } // stages
 }
