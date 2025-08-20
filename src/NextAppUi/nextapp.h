92de4d9d52223b3692d5b3054a039b47614fb42b
Jarle Aase <jarle@jgaa.com>
2025-08-20 12:34:41 +0300

    Added data epoc for client, so we can force a full resync after an upgrade

--- src/NextAppUi/nextapp.h
+++ src/NextAppUi/nextapp.h
@@ -12,6 +12,7 @@
 
 namespace nextapp {
 
+// The epoch for the NextApp data. Used to force a full re-sync after an upgrade if required.
 static constexpr unsigned int app_data_epoc = NEXTAPP_DATA_EPOCH;
 
 } // namespace nextapp
